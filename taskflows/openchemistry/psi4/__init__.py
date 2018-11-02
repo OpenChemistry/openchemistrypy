8470946634c791ecea44b62efb0fed31abc9a3db
Alessandro Genova <alessandro.genova@kitware.com>
2018-11-02 14:13:59 -0400

    Move conditional logic from template to python

--- taskflows/openchemistry/psi4/__init__.py
+++ taskflows/openchemistry/psi4/__init__.py
@@ -9,10 +9,46 @@ class Psi4TaskFlow(OpenChemistryTaskFlow):
         return 'psi4'
 
     def input_generator(self, params, xyz_structure, tmp_file):
+        optimization = params.get('optimization', None)
+        vibrational = params.get('vibrational', None)
+        charge = params.get('charge', 0)
+        multiplicity = params.get('multiplicity', 1)
+        theory = params.get('theory', 'scf')
+        functional = params.get('functional', 'b3lyp')
+        basis = params.get('basis', 'cc-pvdz')
+
+        if optimization is not None:
+            task = 'optimize'
+        elif vibrational is not None:
+            task = 'frequency'
+        else:
+            task = 'energy'
+
+        if theory.lower() == 'dft':
+            _theory = functional
+            reference = 'ks'
+        else:
+            _theory = theory
+            reference = 'hf'
+
+        if multiplicity == 1:
+            reference = 'r' + reference
+        else:
+            reference = 'u' + reference
+
+        context = {
+            'task': task,
+            'theory': _theory,
+            'reference': reference,
+            'charge': charge,
+            'multiplicity': multiplicity,
+            'basis': basis
+        }
+
         template_path = os.path.dirname(__file__)
         jinja2_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_path),
                                         trim_blocks=True)
-        jinja2_env.get_template('oc.psi4.j2').stream(**params, xyz_structure=xyz_structure).dump(tmp_file, encoding='utf8')
+        jinja2_env.get_template('oc.psi4.j2').stream(**context, xyz_structure=xyz_structure).dump(tmp_file, encoding='utf8')
 
     def select_output_files(self, filenames):
         do_copy = [False] * len(filenames)
