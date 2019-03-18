89bab3dbdcc2b3157291e7e1f989f303bcf9afd4
Alessandro Genova <alessandro.genova@kitware.com>
2019-03-18 15:44:32 -0400

    Extract the total energy from nwchem and psi4 calculations

--- openchemistry/io/nwchemJson.py
+++ openchemistry/io/nwchemJson.py
@@ -1,7 +1,9 @@
 import json
 from avogadro.core import Molecule
 from avogadro.io import FileFormatManager
+from jsonpath_rw import parse
 from .base import BaseReader
+from .constants import HARTREE_TO_J_MOL
 
 class NWChemJsonReader(BaseReader):
 
@@ -12,4 +14,10 @@ class NWChemJsonReader(BaseReader):
         conv.read_string(mol, str_data, 'json')
         cjson_str = conv.write_string(mol, 'cjson')
         cjson = json.loads(cjson_str)
+        # Copy some calculated properties
+        data = json.loads(str_data)
+        energy = parse('simulation.calculations[0].calculationResults.totalEnergy.value').find(data)
+        if len(energy) == 1:
+            energy = energy[0].value
+            cjson.setdefault('properties', {})['totalEnergy'] = energy * HARTREE_TO_J_MOL
         return cjson
