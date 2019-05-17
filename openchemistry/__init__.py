ab8dddf7b45c594380eaf11f0fb0334333c78d18
Muammar El Khatib <melkhatibr@lbl.gov>
2019-05-17 14:33:48 -0700

    Added OrcaReader to __init__.py.

--- openchemistry/__init__.py
+++ openchemistry/__init__.py
@@ -1,3 +1,3 @@
 from ._utils import hash_object, camel_to_space, parse_image_name
-from .io import Psi4Reader, NWChemJsonReader, CjsonReader
+from .io import Psi4Reader, NWChemJsonReader, CjsonReader, OrcaReader
 from .api import load, find_structure, find_calculation, find_molecule, monitor, queue, find_spectra
