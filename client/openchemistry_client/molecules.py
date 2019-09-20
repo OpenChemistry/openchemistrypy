4cb8fa3cadc37794f2b6c5105be96ca38d3ee85f
Patrick Avery <patrick.avery@kitware.com>
2019-09-20 15:05:24 -0400

    Added ability to upload molecules from a file
    Molecules can be uploaded from a file like the following:
    ```
    openchemistry-client --api-key $MY_API_KEY molecules upload ethanol.smi
    ```
    
    The user can specify a format, but if a format is not specified,
    it tries to use the file extension.
    
    Also, if the GIRDER_API_KEY environment variable is set, that will
    be used by default for the api key, so it would not need to be
    passed in via the CLI in that case.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- client/openchemistry_client/molecules.py
+++ client/openchemistry_client/molecules.py
@@ -1,5 +1,6 @@
 import click
 import json
+import os
 
 
 _common_help = 'Some kind of help for molecules'
@@ -25,12 +26,37 @@ def _list(gc, limit):
     print(_format_molecules(molecules))
 
 
+@molecules.command('upload')
+@click.argument('filename')
+@click.option('--format', default=None, help='The format of the file')
+@click.pass_obj
+def _upload(gc, filename, format):
+
+    if format is None:
+        format = os.path.splitext(filename)[1][1:].lower()
+
+    with open(filename, 'r') as rf:
+        contents = rf.read()
+
+    body = {
+      format: contents
+    }
+
+    mol = gc.post('/molecules', json=body)
+    print('Molecule uploaded:')
+    print(_format_molecule(mol))
+
+
 def _format_molecules(molecules):
     ret = 'matches: ' + str(molecules['matches']) + '\n'
     ret += 'limit: ' + str(molecules['limit']) + '\n'
     ret += 'offset: ' + str(molecules['offset']) + '\n'
     ret += 'results:\n'
     for res in molecules['results']:
-        ret += json.dumps(res, indent=4) + '\n'
+        ret += _format_molecule(res) + '\n'
 
     return ret
+
+
+def _format_molecule(mol):
+    return json.dumps(mol, indent=4)
