e125cb487e114b052f5d8cdb90bcf1814837c84d
Patrick Avery <patrick.avery@kitware.com>
2019-09-21 10:31:01 -0400

    Added ability to submit and download calculations
    The submission of calculations re-uses functions and classes from
    openchemistry. The girder client that openchemistry is using is
    patched to be the girder client that the openchemistry client is
    using.
    
    An example of an input json file was placed in
    `examples/calculations/input.json`. A molecule ID is required.
    
    Downloading a calculation saves the cjson to an output file.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- client/openchemistry_client/calculations.py
+++ client/openchemistry_client/calculations.py
@@ -1,6 +1,11 @@
 import click
 import json
 
+import openchemistry as oc
+from openchemistry._calculation import CalculationResult
+from openchemistry._calculation import GirderMolecule
+from openchemistry._calculation import PendingCalculationResultWrapper
+
 
 _common_help = 'Some kind of help for calculations'
 
@@ -25,6 +30,48 @@ def _list(gc, limit):
     print(_format_calculations(calcs))
 
 
+@calculations.command('submit')
+@click.argument('input_file')
+@click.pass_obj
+def _submit(gc, input_file):
+
+    # Make sure openchemistry is using our girder client...
+    _patch_oc_girder_client(gc)
+
+    with open(input_file, 'r') as rf:
+        input_dict = json.load(rf)
+
+    molecules = [GirderMolecule(input_dict['molecule_id'])]
+    image_name = input_dict['image_name']
+    input_parameters = input_dict['input_parameters']
+    result = oc.run_calculations(molecules, image_name, input_parameters)[0]
+
+    if isinstance(result, PendingCalculationResultWrapper):
+        print('Calculation running:', result.unwrap()._id)
+    elif isinstance(result, CalculationResult):
+        print('Calculation already performed:', result._id)
+    else:
+        print('Unknown type returned from oc.run_calculations():',
+              type(result))
+
+
+@calculations.command('download')
+@click.argument('calculation_id')
+@click.argument('output_file')
+@click.pass_obj
+def _download(gc, calculation_id, output_file):
+    calcs = gc.get('/calculations/%s' % calculation_id)
+    if 'properties' in calcs and calcs['properties'].get('pending', False):
+        print('Calculation is currently running')
+        return
+
+    print('Downloading calculation:', calculation_id)
+    cjson = calcs.get('cjson', {})
+
+    print('Writing cjson to output file:', output_file)
+    with open(output_file, 'w') as wf:
+        json.dump(cjson, wf)
+
 
 def _format_calculations(calcs):
     ret = 'matches: ' + str(calcs['matches']) + '\n'
@@ -39,3 +86,9 @@ def _format_calculations(calcs):
 
 def _format_calculation(calc):
     return json.dumps(calc, indent=4)
+
+
+def _patch_oc_girder_client(gc):
+    # Make sure the OC girder client is using our client...
+    oc._girder.GirderClient()
+    setattr(oc._girder.GirderClient.__dict__['instance'], 'client', gc)
