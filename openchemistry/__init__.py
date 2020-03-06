c47fb4b35b5f9ae81507788ac2c2fccb7446f3dd
Patrick Avery <patrick.avery@kitware.com>
2020-03-06 14:50:01 -0500

    Add a simple reader for cp2k output
    This can so far only read the final energy and output geometry from
    energy or optimization calculations.
    
    This also generalizes a cclib cjson cleanup function that was
    used in the psi4 reader.
    
    Signed-off-by: Patrick Avery <patrick.avery@kitware.com>

--- openchemistry/__init__.py
+++ openchemistry/__init__.py
@@ -1,7 +1,9 @@
 from ._utils import (
     hash_object, camel_to_space, parse_image_name, calculate_rmsd
 )
-from .io import Psi4Reader, NWChemJsonReader, CjsonReader, OrcaReader
+from .io import (
+    CjsonReader, Cp2kReader, NWChemJsonReader, OrcaReader, Psi4Reader
+)
 from .api import (
     load, find_structure, find_calculation, find_molecule, monitor, queue,
     find_spectra, import_structure, run_calculations
